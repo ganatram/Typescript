0be601940742a90246c558503287cfd52e8890f5
Mukesh <ganatram@gmail.com>
2026-03-20 16:53:10 +0530

    updatedtypescript

---
+++ Annotations.ts
@@ -0,0 +1,19 @@
+// primitive type annotations
+
+//AM  Identifier              : type = 'value'
+/*   let name                    : string = "Steve";
+  let heighInCentimeters      : number = 182.5;
+  let isActive                : boolean = true;
+ */
+
+// Array type annotations
+// const names: [string, string, 350, boolean] = ["Henry", "Smith", 350, false];
+
+// Function type annotations
+/* let sayHello: (name: string) => string;
+
+sayHello = function (name) {
+  return "hello world";
+};
+
+sayHello("hi"); */
--- InferredObjectTypeUnions.ts
+++ InferredObjectTypeUnions.ts
@@ -1,28 +1,17 @@
-const poem =
+/* const poem =
   Math.random() > 0.5
     ? { name: "The Double Image", pages: 7 }
     : { name: "Her Kind", rhymes: true };
-
-/*  
-   
-   {
-      name:string,
-      pages:number,
-      rhymes?:boolean 
-    } 
-
-    |
-
-    {
-    name:string,
-    rhymes:boolean,
-    pages?:number
-    }
-   
-    */
 
 let T1: typeof poem = {
   name: "some string value",
   pages: 100,
   rhymes: false,
 };
+
+let T2: typeof poem = {
+  name: "some string value",
+  rhymes: false,
+  //   pages: 200,
+};
+ */
