2339b83ea8b10838de897d82b1844115b45253f9
Mukesh <ganatram@gmail.com>
2026-06-16 14:42:00 +0530

    Typescriptcodee

---
+++ example1.ts
@@ -0,0 +1,14 @@
+//annotation V/s inference
+/* const firstName = "Georgia"; // type inference
+
+console.log(firstName.length);
+
+let a: number = 100; // type annotation - type explicitly assigned
+let x = 200; // type inference // type inferred based on value assigned.
+
+let y: number = 500; // un-necessary type annotation -- explicitly assigning type was not required.
+ */
+
+// type check into action:
+/* function xyz(firstName: string) {}
+xyz("Beyonce", "Knowles"); // no of aruguments do not match the number of parameters */
