7c90a968cc2a77946b76f3b51f7c53d61cac7172
Mukesh <ganatram@gmail.com>
2026-06-17 10:11:49 +0530

    javascript

--- typeInheritance.ts
+++ typeInheritance.ts
@@ -3,18 +3,56 @@
   born: 2001,
 }; */
 
-type Poet = {
-  // custom type - 'Type alias
+// Type Alias - custom type shape:
+/* type Poet = {
   name: string;
   born: number;
-};
+}; */
+
+// Interface - custom type shape:
+interface Poet {
+  name: string;
+  born: number;
+}
+
+interface Poet {
+  // declarative merging -- interface
+  job: string;
+}
 
 const PoetLater: Poet = {
   name: "Henry",
   born: 2000,
+  job: "singer",
 };
 
 const YetAnotherPoet: Poet = {
   name: "Freeman",
   born: 2010,
+  job: "director",
 };
+
+// Reusing type shapes with 'typeof' operator
+/* const PoetLater: typeof Poet = {
+  name: "Henry",
+  born: 2000,
+};
+
+
+const YetAnotherPoet: typeof Poet = {
+  name: "Freeman",
+  born: 2010,
+}; */
+
+/* 
+type Poet = {  // custom type - 'Type alias'
+  name: string;
+  born: number;
+};
+
+
+const YetAnotherPoet: Poet = {
+  name: "Freeman",
+  born: 2010,
+};
+ */
